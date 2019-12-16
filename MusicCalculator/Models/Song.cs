c08575dc1599d593e95401046aa27f16c6a2466f
erhed <erik.hedrenius@gmail.com>
2019-12-16 17:00:03 +0100

    Add and list songs

--- MusicCalculator/Models/Song.cs
+++ MusicCalculator/Models/Song.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using SQLite;
 
-namespace MusicCalculator
+namespace MusicCalculator.Models
 {
     public class Song
     {        [PrimaryKey, AutoIncrement]        public int ID { get; set; }        public string Title { get; set; }        public int Tempo { get; set; }    }
--- MusicCalculator/Pages/SongPage.xaml.cs
+++ MusicCalculator/Pages/SongPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using MusicCalculator.Models;
+using MusicCalculator.Data;
 
 using Xamarin.Forms;
 
@@ -7,10 +9,77 @@ namespace MusicCalculator
 {
     public partial class SongPage : ContentPage
     {
+        public List<Song> Songs;
+
         public SongPage()
         {
             InitializeComponent();
+
             Title = "Songs";
+            ((NavigationPage)Application.Current.MainPage).BarBackgroundColor = Color.FromHex("#233951");
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Songs = await App.Database.GetSongsAsync();
+            RenderSongs();
+        }
+
+        private void RenderSongs()
+        {
+            for (int i = 0; i < Songs.Count; i++)
+            {
+                Label titleLabel = new Label();
+                titleLabel.Text = Songs[i].Title;
+                titleLabel.TextColor = Color.FromHex("#FFFFFF");
+                Grid.SetColumn(titleLabel, 0);
+                Grid.SetRow(titleLabel, i);
+                Song_Grid.Children.Add(titleLabel);
+
+                Label tempoLabel = new Label();
+                tempoLabel.Text = Songs[i].Tempo.ToString("0");
+                tempoLabel.TextColor = Color.FromHex("#FFFFFF");
+                tempoLabel.HorizontalTextAlignment = TextAlignment.End;
+                Grid.SetColumn(tempoLabel, 1);
+                Grid.SetRow(tempoLabel, i);
+                Song_Grid.Children.Add(tempoLabel);
+            }
+
+            Label spaceLabel = new Label();
+            spaceLabel.Text = "";
+            Grid.SetRow(spaceLabel, Songs.Count);
+            Song_Grid.Children.Add(spaceLabel);
+        }
+
+        private void ClearSongs()
+        {
+            Song_Grid.Children.Clear();
+        }
+
+        private async void UpdateSongs()
+        {
+            ClearSongs();
+            Songs = await App.Database.GetSongsAsync();
+            RenderSongs();
+        }
+
+        async void OnAddButtonClicked(object sender, EventArgs e)
+        {
+            string tempoString = Tempo_Entry.Text;
+            int tempoInt = Convert.ToInt32(tempoString);
+            var song = new Song
+            {
+                Title = Title_Entry.Text,
+                Tempo = tempoInt
+            };
+            await App.Database.SaveSongAsync(song);
+
+            Title_Entry.Text = "";
+            Tempo_Entry.Text = "";
+
+            UpdateSongs();
         }
     }
 }
